e7bfdc543549ce133c397ff9d1f7fc9b046b7d85
Selcuk <devselcukdev@gmail.com>
2022-01-09 01:09:32 +0300

    Initial Commit

---
+++ xxxad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xxxad
+//
+//  Created by MacMini on 9.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
